3cbc5e03eabbbc9cfffefa82110c1fcc45657563
MorganKryze <neoyega@icloud.com>
2025-01-07 00:05:45 +0100

    trying not to load modules

--- src/controller.py
+++ src/controller.py
@@ -40,7 +40,7 @@ from apps_v2 import (
     life,
     #spotify_player,
 )
-from modules import weather_module, notification_module, spotify_module
+#from modules import weather_module, notification_module, spotify_module
 
 sw = 13
 enc_A = 5
@@ -112,9 +112,9 @@ def main():
     }
 
     modules = {
-        "weather": weather_module.WeatherModule(config),
-        "notifications": notification_module.NotificationModule(config),
-        "spotify": spotify_module.SpotifyModule(config),
+        # "weather": weather_module.WeatherModule(config),
+        # "notifications": notification_module.NotificationModule(config),
+        # "spotify": spotify_module.SpotifyModule(config),
     }
 
     app_list = [
