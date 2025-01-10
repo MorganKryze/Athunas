fae0c2420be9c9461ff1bb8429413537ec6bec8f
MorganKryze <neoyega@icloud.com>
2025-01-10 12:20:13 +0100

    🚑 tempfix: logs not stored

--- src/utils.py
+++ src/utils.py
@@ -24,8 +24,8 @@ class Utils:
             level=level,
             format="%(asctime)s - %(levelname)s - %(message)s",
             datefmt="%Y-%m-%d %H:%M:%S",
-            filename=log_file_path,
-            filemode="a",
+            # filename=log_file_path,
+            # filemode="a",
         )
         logging.info("-------------------------------------------------------------")
         logging.info("[Utils] Application started.")
