7d5d864bd8b794edf8a0eba03727bfd918a8364d
MorganKryze <neoyega@icloud.com>
2025-01-11 01:29:51 +0100

    🚑 fix: remove root logger to add consistence

--- src/utils.py
+++ src/utils.py
@@ -45,20 +45,26 @@ class Utils:
 
         logger = logging.getLogger()
         logger.setLevel(level)
-    
+
+        # Remove any existing handlers to avoid conflicts
+        if logger.hasHandlers():
+            logger.handlers.clear()
+
         file_handler = logging.FileHandler(log_file_path)
         console_handler = logging.StreamHandler()
 
         file_handler.setLevel(level)
         console_handler.setLevel(level)
 
-        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+        )
         file_handler.setFormatter(formatter)
         console_handler.setFormatter(formatter)
 
         logger.addHandler(file_handler)
         logger.addHandler(console_handler)
-        
+
         logging.info("-------------------------------------------------------------")
         logging.info("[Utils] Application started.")
 
